ec24779d3a88654be8529591ecc7444e35902686
cichocj0 <cichocjagh@gmail.com>
2024-11-14 20:40:03 +0100

    Solve 08-calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+  return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator * currentValue);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  for (let i = 1; i <= x; ++i) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
