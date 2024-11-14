18052f44a9d4fc8b3750675d0687d4831455272f
cichocj0 <cichocjagh@gmail.com>
2024-11-14 21:16:27 +0100

    Solve 10_fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(member) {
+    if (member == 0) return 0;
 
+    if (member < 0) return "OOPS"; 
+
+    const fibonacciArray = [1, 1];
+
+    for (let i = 2; i < member; ++i) {
+        fibonacciArray[i] = fibonacciArray[i - 1] + fibonacciArray[i - 2];
+    }
+
+    return fibonacciArray[fibonacciArray.length - 1];
 };
 
 // Do not edit below this line
