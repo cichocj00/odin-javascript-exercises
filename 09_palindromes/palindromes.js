fc3594e86b46f6d0a1876f39d937c87be58c9200
cichocj0 <cichocjagh@gmail.com>
2024-11-14 21:05:15 +0100

    Solve 09_palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    const regex = /[\W_]/g;
+    const lowRegStr = text.toLowerCase().replace(regex, '');
+    const reversedText = lowRegStr.split('').reverse().join('');
 
+    return reversedText === lowRegStr;
 };
 
 // Do not edit below this line
